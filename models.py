71801e672c319e7f6802352c68fa5023ae9178b1
Sowmya2001 <54609768+Sowmya2001@users.noreply.github.com>
2025-04-23 11:05:19 +0530

    models.py

--- models.py
+++ models.py
@@ -1,29 +1,11 @@
-# import pandas as pd
-# from sklearn.model_selection import train_test_split
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.svm import LinearSVC
-# from joblib import dump
 
-# def train_model():
-#     df = pd.read_csv("data/emails.csv")
-#     X = df['email']
-#     y = df['type']
-
-#     vectorizer = TfidfVectorizer(stop_words='english')
-#     X_vect = vectorizer.fit_transform(X)
-
-#     clf = LinearSVC()
-#     clf.fit(X_vect, y)
-
-#     dump(vectorizer, "saved_models/vectorizer.joblib")
-#     dump(clf, "saved_models/model.joblib")
 
 import os
 import joblib
 
 # Correct paths
-model_path = "saved_models/model.joblib"
-vectorizer_path = "saved_models/vectorizer.joblib"
+model_path = "model.joblib"
+vectorizer_path = "vectorizer.joblib"
 
 # Load model and vectorizer
 if os.path.exists(model_path) and os.path.exists(vectorizer_path):
